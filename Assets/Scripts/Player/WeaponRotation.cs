d8ac82d8e39dc5f5f778a8117778f5e14aacf3f3
Yuri Katayama <tooketsu@gmail.com>
2018-11-07 20:50:00 -0200

    Update Assets/Scripts/Player/WeaponRotation.cs

--- Assets/Scripts/Player/WeaponRotation.cs
+++ Assets/Scripts/Player/WeaponRotation.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponRotation : MonoBehaviour {
-
-	public int rotationOffSet  = 90;
+ 
+	public int rotationOffSet  = 0;
 	
 	// Update is called once per frame
 	void Update () {
@@ -15,11 +15,9 @@ public class WeaponRotation : MonoBehaviour {
 		float rotZ = Mathf.Atan2 (diff.y, diff.x) * Mathf.Rad2Deg;
 		Debug.Log ("Angle: " + rotZ);
 
-		if (rotZ < 80 && rotZ > -80) {
-			transform.rotation = Quaternion.Euler (0f, 0f, rotZ - rotationOffSet);
-		}
-	//	transform.rotation = Quaternion.Euler (0f, 0f, rotZ - rotationOffSet);
-
-
+		transform.rotation = Quaternion.Euler (0f, 0f, rotZ - rotationOffSet);
+		
 	}
+
+	
 }
